4f04888bee05a6c6eb4142fa76ecc9f6cd0ff670
slavo@mimicom24.com <slavo@mimicom24.com>
2017-05-18 23:29:13 -0400

    add int

--- app.py
+++ app.py
@@ -2,6 +2,6 @@ print("Guess a number from 1 to 10!!!")
 print("******************************")
 a = input("Please enter a number: ")
 a = int(a)
-b = randint(1, 10)
+b = random.randint(1, 10)
 
 print(b)
