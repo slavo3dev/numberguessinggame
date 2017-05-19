561e7c2faa3f00f5733b955c23361e207180999c
slavo@mimicom24.com <slavo@mimicom24.com>
2017-05-18 23:28:30 -0400

    random number

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 print("Guess a number from 1 to 10!!!")
 print("******************************")
-a = input("Please enter a number!!!")
+a = input("Please enter a number: ")
 a = int(a)
-b = random()
+b = randint(1, 10)
 
 print(b)
