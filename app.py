aa4ccc93026dc621782f0a53179deb4ca85afccf
slavo@mimicom24.com <slavo@mimicom24.com>
2017-05-18 23:40:49 -0400

    working

--- app.py
+++ app.py
@@ -2,15 +2,17 @@ import random
 
 print("Guess a number from 1 to 10!!!")
 print("******************************")
-def game():
-    a = input("Please enter a number: ")
+a = input("Please enter a number: ")
 a = int(a)
-game()
 b = random.randint(1, 10)
+def game():
+    if (a == b):
+        print("You are a winner your number is ", a)
+    else:
+        print("Please try again")
+game()
 
-if (a == b):
-    print("You are a winner your number is ", a)
-else:
-    print("Please try again")
-    game()
+
+
+    
 
