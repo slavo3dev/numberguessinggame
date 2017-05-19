575812d4be2051b9425fc3f79aae5ed3ee408902
slavo@mimicom24.com <slavo@mimicom24.com>
2017-05-18 23:36:26 -0400

    adding functionality

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import random
 
 print("Guess a number from 1 to 10!!!")
 print("******************************")
-def game:
+def game():
     a = input("Please enter a number: ")
 a = int(a)
 game()
@@ -10,7 +10,7 @@ b = random.randint(1, 10)
 
 if (a == b):
     print("You are a winner your number is ", a)
-    else:
-        print("Please try again")
-        game()
-        
+else:
+    print("Please try again")
+    game()
+
