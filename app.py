5f66af6e047bf397af14aa0ccc05fcbea832bec3
slavo@mimicom24.com <slavo@mimicom24.com>
2017-05-19 23:27:43 -0400

    Guessing game

--- app.py
+++ app.py
@@ -1,15 +1,40 @@
 import random
 
-print("Guess a number from 1 to 10!!!")
+print("Guess a number from 1 to 20!!!")
 print("******************************")
-a = input("Please enter a number: ")
+print("You will have tree  opportunities")
+a = input("Eneter a number: ")
 a = int(a)
-b = random.randint(1, 10)
+b = random.randint(1, 20)
 def game():
     if (a == b):
-        print("You are a winner your number is ", a)
+        print("You are a Winner your number you guess is:  ", b)
+    elif(b > 10):
+        print("Number is bigger then 10 please try again")
+        c = input("Try again, Enter a number: ")
+        c = int(c)
+    elif(b < 10):
+        print("Number is smoller then 10 please try again")
+        c = input("Try again, Enter a number: ")
+        c = int(c)
+    elif (c == b):
+        print ("Well Done, you did it!!! Hidden number is: ", b)
+    elif (b > 15):
+        print("Number is bigger then 15 please try again")
+        d = input("Last try, eneter a number: ")
+        d = int(d)
+    elif (b < 5):
+        print("Number is  smoller then 15 please try again")
+        d = input("Last try, eneter a number: ")
+        d = int(d)
+    elif ( b > 5 and b < 15):
+        print("Number is  bigger then 5 but smaller then 15")
+        d = input("Last try, eneter a number: ")
+        d = int(d)
+    elif (d == b):
+        print("Great, You did it!!Well Done! Hidden number is: ", b)
     else:
-        print("Please try again")
+        print ("Try again... don't give up! Hidden number is: ", b)
 game()
 
 
